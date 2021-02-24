7f47a6747ec652ea3c134a22289e3de600575cde
warrior-coder <gudkou2003@gmail.com>
2021-02-24 18:30:25 +0300

    Optimize moves

--- script.js
+++ script.js
@@ -3,22 +3,22 @@ const RED = 'rgba(255, 68, 0, 0.8)';
 const GREEN = 'rgba(153, 205, 50, 0.8)';
 const YELLOW = 'rgba(255, 255, 0, 0.9)';
 const _FIGURES = [
-    {type: -4, x: 0, y: 0},{type: -2, x: 1, y: 0},{type: -3, x: 2, y: 0},{type: -9, x: 3, y: 0},{type: -999, x: 4, y: 0},{type: -3, x: 5, y: 0},{type: -2, x: 6, y: 0},{type: -4, x: 7, y: 0},
+    {type: -5, x: 0, y: 0},{type: -3, x: 1, y: 0},{type: -4, x: 2, y: 0},{type: -9, x: 3, y: 0},{type: -999, x: 4, y: 0},{type: -4, x: 5, y: 0},{type: -3, x: 6, y: 0},{type: -5, x: 7, y: 0},
     {type: -1, x: 0, y: 1},{type: -1, x: 1, y: 1},{type: -1, x: 2, y: 1},{type: -1, x: 3, y: 1},{type: -1, x: 4, y: 1},{type: -1, x: 5, y: 1},{type: -1, x: 6, y: 1},{type: -1, x: 7, y: 1},
     {type: 1, x: 0, y: 6},{type: 1, x: 1, y: 6},{type: 1, x: 2, y: 6},{type: 1, x: 3, y: 6},{type: 1, x: 4, y: 6},{type: 1, x: 5, y: 6},{type: 1, x: 6, y: 6},{type: 1, x: 7, y: 6},
-    {type: 4, x: 0, y: 7},{type: 2, x: 1, y: 7},{type: 3, x: 2, y: 7},{type: 9, x: 3, y: 7},{type: 999, x: 4, y: 7},{type: 3, x: 5, y: 7},{type: 2, x: 6, y: 7},{type: 4, x: 7, y: 7},
+    {type: 5, x: 0, y: 7},{type: 3, x: 1, y: 7},{type: 4, x: 2, y: 7},{type: 9, x: 3, y: 7},{type: 999, x: 4, y: 7},{type: 4, x: 5, y: 7},{type: 3, x: 6, y: 7},{type: 5, x: 7, y: 7},
 ];
 var i2 = -1, j2 = -1, figureActive = false;
 
 var _BOARD = [
-    [-4, -2, -3, -9, -999, -3, -2, -4],
+    [-5, -3, -4, -9, -999, -4, -3, -5],
     [-1, -1, -1, -1, -1, -1, -1, -1],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1],
-    [4, 2, 3, 9, 999, 3, 2, 4],
+    [5, 3, 4, 9, 999, 4, 3, 5],
 ];
 console.log(_BOARD);
 
@@ -29,13 +29,9 @@ vue1 = new Vue({
         board: [],
         figures: [],
         history: [],
-        activeCell: false,
-        turn: 1,
-        i_glow2: -1,
-        j_glow2: -1,
-        i_glow: -1,
-        j_glow: -1,
         freeMoves: [],
+        glowCell: {gx: -1, gy: -1, gc: YELLOW, glow: false},
+        turn: 1,
     },
 
     beforeMount() {
@@ -48,15 +44,10 @@ vue1 = new Vue({
         {
             if (this.turn > 0 && this.figures[fugure_k].type > 0)
             {
-                
                 this.freeMoves = this.findMoves(this.board, i, j);
-                
-                // let sump = 0;
-                // this.freeMoves.forEach(fm => {
-                //     if(fm.mp != 0) sump += fm.mp
-                // });
-                // console.log(sump);
-
+                this.glowCell.gx = j;
+                this.glowCell.gy = i;
+                this.glowCell.glow = true;
                 i2 = i;
                 j2 = j;
                 figureActive = fugure_k;
@@ -64,194 +55,149 @@ vue1 = new Vue({
             else if (figureActive)
             {
                 this.freeMoves.splice(0);
+                this.glowCell.glow = false;
                 figureActive = 0;
             }
         },
         cellAction(ci, cj, ck)
         {
-            if(this.freeMoves[ck].mc == GREEN || this.freeMoves[ck].mc == RED)
+            if (figureActive)
             {
-                if (figureActive)
-                {
-                    /* IF BEAT */
-                    if (this.freeMoves[ck].mp != 0)
-                    {
-                        let index = this.figures.findIndex(element => {
-                            if(element.x == cj && element.y == ci && element.type < 0) return true;
-                        });
-                        this.figures[index].type = 0;
-                    }
 
-                    this.figureMove(figureActive, ci, cj, i2, j2); // MAKE MOVE
+                this.figureMove(ci, cj, i2, j2); // MAKE MOVE
 
-                    this.freeMoves.splice(0);
-                    figureActive = 0;
-                    this.turn *= -1;
+                this.freeMoves.splice(0);
+                figureActive = 0;
+                this.glowCell.glow = false;
+                
+                this.turn *= -1;
 
-                    /* BOT TIME*/
-                    this.botAction();
-                }
+                /* BOT TIME*/
+                this.botAction();
             }
         },
         botAction()
         {
-            let blackFigIds = [];
-            for (let k = 0; k < 16; k++)
-                if (this.figures[k].type < 0) blackFigIds.push(k); // COLLECT ALL BLACK FIGURES
-            
-            let bestBlackFigId = -1, i_best = -1, j_best = -1, bestPoints = -9999;
+            let bestBlackFig = -1, i_best = -1, j_best = -1, bestPoints = -9999;
 
-            blackFigIds.forEach(blackFigId =>
+            let blackFigs = this.CollectSideFigures(this.board, -1);
+            
+
+            blackFigs.forEach(blackFig =>
             {
-                let botMoves = this.findMoves(this.board, this.figures[blackFigId].y, this.figures[blackFigId].x); // GENERATE BOT MOVES
+                let botMoves = this.findMoves(this.board, blackFig.i, blackFig.j); // GENERATE BOT MOVES
 
                 botMoves.forEach(botMove =>
                 {
 
                     /* MAKE FAKE MOVE */
                     let tempFig = this.board[botMove.mi][botMove.mj];
-                    this.board[botMove.mi][botMove.mj] = this.figures[blackFigId].type;
-                    this.board[this.figures[blackFigId].y][this.figures[blackFigId].x] = 0;
+                    this.board[botMove.mi][botMove.mj] = blackFig.type;
+                    this.board[blackFig.i][blackFig.j] = 0;
 
 
                     /* PLAYER POINTS */
-                    
-                    let blackPoints = botMove.mp;
-                    let whitePoints = this.CountSidePoints(this.board, 1);
+                    let points = botMove.mp + this.CountSidePoints(this.board, 1);
 
-                    if(whitePoints + blackPoints > bestPoints)
+                    if(points > bestPoints)
                     {
-                        bestBlackFigId = blackFigId;
+                        bestBlackFig = blackFig;
                         i_best = botMove.mi;
                         j_best = botMove.mj;
-                        bestPoints = whitePoints + blackPoints;
+                        bestPoints = points;
                     }
-                    else if(whitePoints + blackPoints == bestPoints)
+                    else if(points == bestPoints)
                     {
-                        if (blackFigId > bestBlackFigId)
+                        if (blackFig.type > bestBlackFig.type)
                         {
-                            bestBlackFigId = blackFigId;
+                            bestBlackFig = blackFig;
                             i_best = botMove.mi;
                             j_best = botMove.mj;
-                            bestPoints = whitePoints + blackPoints;
+                            bestPoints = points;
                         }
                     }
 
                     /* RETURN FAKE MOVE */
                     this.board[botMove.mi][botMove.mj] = tempFig;
-                    this.board[this.figures[blackFigId].y][this.figures[blackFigId].x] = this.figures[blackFigId].type;
+                    this.board[blackFig.i][blackFig.j] = blackFig.type;
                 });
 
-                
-
             });
-            console.log('FIG:', bestBlackFigId);
+            console.log('FIG:', bestBlackFig);
             console.log('BEST MOVE:', j_best, i_best);
             console.log('BEST POINTS', bestPoints);
 
-
-            let bi = i_best;
-            let bj = j_best;
-            let bi2 = this.figures[bestBlackFigId].y;
-            let bj2 = this.figures[bestBlackFigId].x;
-
-            /* IF BEAT */
+            this.figureMove(i_best, j_best, bestBlackFig.i, bestBlackFig.j); // MAKE MOVE
             
-            for (let k = 16; k < 32; k++)
-            {
-                if (this.figures[k].y == i_best && this.figures[k].x == j_best && this.figures[k].type > 0)
-                {
-                    this.figures[k].type = 0;
-                    break;
-                }
-            }
-
-            this.figureMove(bestBlackFigId, bi, bj, bi2, bj2); // MAKE RANDOM MOVE
-
-
-            // if(blackFigures.length == 0) // IF NO BLACK FIGURES
-            // {
-            //     this.turn *= -1;
-            //     break;
-            // }
-
-            // let randFigure = blackFigures[Math.floor(Math.random() * (blackFigures.length-1))] // CHOOSE RANDOM BLACK FIGURE
-            // console.log('TRY:', randFigure, ':', this.figures[randFigure].x, this.figures[randFigure].y);
-            
-            // let botMoves = this.findMoves(this.board, this.figures[randFigure].y, this.figures[randFigure].x); // GENERATE BOT MOVES
-            // let nMoves = botMoves.length;
-
-            // if(nMoves > 1) // IF HAS MOVES
-            // {
-            //     console.log('---MOVE---');
-
-            //     let sump = 0;
-            //     botMoves.forEach(fm => {
-            //         if(fm.mp != 0) sump += fm.mp
-            //     });
-            //     console.log(sump);
-
-                
-            //     this.turn *= -1;
-            // }
-
             this.turn *= -1;
+        },
+        CollectSideFigures(board, side)
+        {
+            let sideFigures = [];
+            for (let i = 0; i < 8; i++)
+                for (let j = 0; j < 8; j++)
+                    if (board[i][j] * side > 0)
+                    {
+                        sideFigures.push({i: i, j: j, type: board[i][j]});
+                    }
+
+            return sideFigures;
         },
         CountSidePoints(board, side)
         {
-            let sideFigures = [], sidePoints = 0;
-
-            if (side == -1)
-            {
-                for (let k = 0; k < 16; k++)
-                    if (this.figures[k].type < 0) sideFigures.push(k); // COLLECT ALL BLACK FIGURES
-            }
-            else 
-            {
-                for (let k = 16; k < 32; k++)
-                    if (this.figures[k].type > 0) sideFigures.push(k); // COLLECT ALL WHITE FIGURES
-            }
+            let sideFigures = this.CollectSideFigures(board, side), sidePoints = 0;
           
-            sideFigures.forEach(FigId =>
+            sideFigures.forEach(sideFig =>
             {
-                let moves = this.findMoves(this.board, this.figures[FigId].y, this.figures[FigId].x); // GENERATE MOVES
-
+                let moves = this.findMoves(board, sideFig.i, sideFig.j); // GENERATE MOVES
                 moves.forEach( move =>
                 {
-                    sidePoints += move.mp
+                    sidePoints += move.mp;
                 });
 
             });
 
             return sidePoints;
         },
-        figureMove(FigureId, fi, fj, fi2, fj2) {
-
+        figureMove(i_to, j_to, i_from, j_from) {
+            /* BEAT */
+            if(this.board[i_to][j_to] != 0)
+            {
+                let index = this.figures.findIndex(element => {
+                    if(element.x == j_to && element.y == i_to && element.type != 0) return true;
+                });
+                this.figures[index].type = 0;
+            }
+        
             /* MOVE */
-            this.figures[FigureId].x = fj;
-            this.figures[FigureId].y = fi;
-            this.board[fi][fj] = this.board[fi2][fj2];
-            this.board[fi2][fj2] = 0;
+            let index = this.figures.findIndex(element => {
+                if(element.x == j_from && element.y == i_from && element.type != 0) return true;
+            });
+
+            this.figures[index].y = i_to;
+            this.figures[index].x = j_to;
+
+            this.board[i_to][j_to] = this.board[i_from][j_from];
+            this.board[i_from][j_from] = 0;
 
             /* HISTORY */
-            this.history[0] = {hx: fj, hy: fi, hc: YELLOW};
-            this.history[1] = {hx: fj2, hy: fi2, hc: YELLOW};
+            this.history[0] = {hx: j_to, hy: i_to, hc: YELLOW};
+            this.history[1] = {hx: j_from, hy: i_from, hc: YELLOW};
 
             /* WHITE QUEEN CHECK */
-            if(this.figures[FigureId].type == 1 && fi == 0) 
+            if(this.figures[index].type == 1 && i_to == 0) 
             {
-                this.figures[FigureId].type = 9; 
-                this.board[fi][fj] = 9;
+                this.figures[index].type = 9; 
+                this.board[i_to][j_to] = 9;
             }
 
             /* BLACK QUEEN CHECK */
-            else if(this.figures[FigureId].type == -1 && fi == 7) 
+            else if(this.figures[index].type == -1 && i_to == 7) 
             {
-                this.figures[FigureId].type = -9; 
-                this.board[fi][fj] = -9;
+                this.figures[index].type = -9; 
+                this.board[i_to][j_to] = -9;
             }
         },
-    
         findMoves(board, i, j)
         {
             let findMoves = [];
@@ -272,8 +218,8 @@ vue1 = new Vue({
                     if (i-1 > -1 && j+1 < 8 && board[i-1][j+1] < 0) findMoves.push({mi: i-1, mj: j+1, mp: board[i-1][j+1], mc: RED}); // BEAT
                 break;
                 /* BISHOP */
-                case -3:
-                case 3:
+                case -4:
+                case 4:
                     for(let k = 1; k < 8 && i+k < 8 && j+k < 8; k++)
                     {
                         if(board[i+k][j+k] == 0) findMoves.push({mi: i+k, mj: j+k, mp: 0, mc: GREEN});
@@ -312,8 +258,8 @@ vue1 = new Vue({
                     } // UP RIGHT
                 break;
                 /* ROOK */
-                case -4:
-                case 4:
+                case -5:
+                case 5:
                     for(let k = 1; k < 8 && i+k < 8; k++)
                     {
                         if(board[i+k][j] == 0) findMoves.push({mi: i+k, mj: j, mp: 0, mc: GREEN});
@@ -364,16 +310,16 @@ vue1 = new Vue({
                     }
                 break;
                 /* KNIGHT */
-                case -2:
-                case 2:
+                case -3:
+                case 3:
                     for(let k = -1; k < 2; k += 2)
                     {
                         for(let l = -2; l < 3; l += 4)
                         {
-                            if(i+k > -1 && i+k < 7 && j+l > -1 && j+l < 8 && board[i+k][j+l] == 0) findMoves.push({mi: i+k, mj: j+l, mp: 0, mc: GREEN});
-                            if(i+k > -1 && i+k < 7 && j+l > -1 && j+l < 8 && board[i+k][j+l] * board[i][j] < 0) findMoves.push({mi: i+k, mj: j+l, mp: board[i+k][j+l], mc: RED});
-                            if(i+l > -1 && i+l < 7 && j+k > -1 && j+k < 8 && board[i+l][j+k] == 0) findMoves.push({mi: i+l, mj: j+k, mp: 0, mc: GREEN});
-                            if(i+l > -1 && i+l < 7 && j+k > -1 && j+k < 8 && board[i+l][j+k] * board[i][j] < 0) findMoves.push({mi: i+l, mj: j+k, mp: board[i+l][j+k], mc: RED});
+                            if(i+k > -1 && i+k < 8 && j+l > -1 && j+l < 8 && board[i+k][j+l] == 0) findMoves.push({mi: i+k, mj: j+l, mp: 0, mc: GREEN});
+                            if(i+k > -1 && i+k < 8 && j+l > -1 && j+l < 8 && board[i+k][j+l] * board[i][j] < 0) findMoves.push({mi: i+k, mj: j+l, mp: board[i+k][j+l], mc: RED});
+                            if(i+l > -1 && i+l < 8 && j+k > -1 && j+k < 8 && board[i+l][j+k] == 0) findMoves.push({mi: i+l, mj: j+k, mp: 0, mc: GREEN});
+                            if(i+l > -1 && i+l < 8 && j+k > -1 && j+k < 8 && board[i+l][j+k] * board[i][j] < 0) findMoves.push({mi: i+l, mj: j+k, mp: board[i+l][j+k], mc: RED});
                         }
                     }
                 break;
